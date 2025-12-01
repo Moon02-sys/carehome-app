7c2f7fdd994f91b7dd9935ac1b988f6e9e067365
Moon <65020220+Moon02-sys@users.noreply.github.com>
2025-12-01 15:48:15 +0100

    cambios en settings y en el json

--- config/settings.py
+++ config/settings.py
@@ -81,14 +81,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'isanchez$carehome',
-        'USER': 'isanchez',
-        'PASSWORD': 'mysql123',
-        'HOST': 'isanchez.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
